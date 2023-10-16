c814ffdc176f3c6974dc553fcd38e92d9d93f04f
Samuel Kori <samuel@wearequantum.co.uk>
2023-10-16 22:19:33 +0100

    adsense

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -7,6 +8,8 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4444864222461908"
+     crossorigin="anonymous"></Script>
       </body>
     </Html>
   )
